f16f88431bad2f904e1824f9b0e8119f3cbf1cba
jeckacodes <jess.bainbridgesmith@gmail.com>
2019-10-21 15:42:51 -0700

    Bare Minimum Requirements Pass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, element) {
   // your code here
+  var results = [];
+
+  var helper = function (node) {
+    var final = node.className.split(' ');
+    if (final.indexOf(className) >= 0) {
+      results.push(node);
+    }
+    if (node.hasChildNodes()) {
+      for (var i = 0; i < node.children.length; i++) {
+        helper(node.children[i]);
+      }
+    }
+  };
+
+  helper(document.body);
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,44 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'boolean' || typeof obj === 'number') {
+    return '' + obj;
+  }
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    var first = '[';
+    for (var i = 0; i < obj.length; i ++) {
+      if (i === obj.length - 1) {
+        first += stringifyJSON(obj[i]) + ']';
+      } else {
+        first += stringifyJSON(obj[i]) + ',';
+      }
+    }
+    return first;
+  } else if (obj instanceof Object) {
+    var array = Object.keys(obj);
+    if (array.length === 0) {
+      return '{}';
+    }
+    var firstObj = '{';
+    for (var i = 0; i < array.length; i ++) {
+      if (obj[array[i]] === undefined || typeof obj[array[i]] === 'function') {
+        return '{}';
+      }
+      if (i === array.length - 1) {
+        firstObj += stringifyJSON(array[i]) + ':' + stringifyJSON(obj[array[i]]) + '}';
+      } else {
+        firstObj += stringifyJSON(array[i]) + ':' + stringifyJSON(obj[array[i]]) + ',';
+      }
+    }
+    return firstObj;
+  }
 };
